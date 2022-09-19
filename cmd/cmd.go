6fb33559aff96cd7b0c94da7013d4889e9bf8f5e
vyacheslav.morozov <verdark768@yandex.ru>
2022-09-19 12:09:09 +0300

    first

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,12 +1,15 @@
 package cmd
 
-import "test/config"
+import (
+	"github.com/ihatiko/log"
+	"test/config"
+)
 
 func Run() {
 	cfg, err := config.GetConfig()
 	if err != nil {
 		panic(err)
 	}
-
+	log.FatalF("%s", "key")
 	cfg = cfg
 }
